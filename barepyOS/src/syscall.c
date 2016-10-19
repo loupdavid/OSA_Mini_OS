fa802bace5c221f2dbe59ce765b5cc86918f2e18
David LOUP <david.loup@insa-lyon.fr>
2016-10-19 14:31:20 +0200

    do_sys_reboot working

--- barepyOS/src/syscall.c
+++ barepyOS/src/syscall.c
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "asm_tools.h"
 // Fonction appel sys depuis le user space
 void sys_reboot(){
   //r0 = 1 : reboot
@@ -7,6 +8,13 @@ void sys_reboot(){
 }
 
 void do_sys_reboot(){
+  const int PM_RSTC = 0x2010001c;
+  const int PM_WDOG = 0x20100024;
+  const int PM_PASSWORD = 0x5a000000;
+  const int PM_RSTC_WRCFG_FULL_RESET = 0x00000020;
+  Set32(PM_WDOG, PM_PASSWORD | 1);
+  Set32(PM_RSTC, PM_PASSWORD | PM_RSTC_WRCFG_FULL_RESET);
+  while(1); 
 }
 
 //Software Interrupt handler (kernel space)
